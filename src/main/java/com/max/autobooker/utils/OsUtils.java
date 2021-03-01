c65770557393320853c85aa56b4ecc59c52e6769
Maxime Rocchia <maxime@friartuck.net>
2021-03-01 12:33:22 +0800

    Add OS Utils

--- src/main/java/com/max/autobooker/utils/OsUtils.java
+++ src/main/java/com/max/autobooker/utils/OsUtils.java
@@ -18,4 +18,14 @@ package com.max.autobooker.utils;
  * @author Maxime Rocchia
  */
 public class OsUtils {
+
+    private static String OS = System.getProperty("os.name").toLowerCase();
+
+    public static boolean isWindows() {
+        return (OS.indexOf("win") >= 0);
+    }
+
+    public static boolean isMac() {
+        return (OS.indexOf("mac") >= 0);
+    }
 }
